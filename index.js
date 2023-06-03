4154af0dd09f0ef70c9321cfa369aedeee802c37
Gurkan Yakar <data.yakar@gmail.com>
2023-06-03 19:24:13 +0300

    connect and disconnect transactions using socketio

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-// const http = require('http');
-
-// const server = http.createServer((req,res) => {
-//     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-//     res.write('Successfull connection ! ');
-//     res.end();
-// });
-
-// server.listen(3000, () => {
-//     console.log('App started !')
-// })
-
-
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
+const path = require('path');
 
 const app = express();
+const server = http.createServer(app);
+const socketio = socket(server);
+const port = 3000;
 
-app.get('/', (req,res) => {
-    res.send('test message ... !');
-})
+//start index html
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
 
-app.get('/chat', (req, res) => {
-    res.send('you are in chat page...!');
-   });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
-app.listen(3000, ()=> {
+const onlineUsers = new Map(); //online user list
+
+socketio.on('connection', (socket) => {
+    socket.on('join', (username) => {
+        onlineUsers.set(socket.id, username); // add new user to list
+        console.log(username + ' connected to server..!')
+        socketio.emit('userList', Array.from(onlineUsers.values()));
+    });
+    //client disconnect delete from list
+    socket.on('disconnect', () => {
+        const username = onlineUsers.get(socket.id); // get username using socket id
+        onlineUsers.delete(socket.id); //delete user to list
+        console.log(username + ' disconnected to server..!')
+        socketio.emit('userList', Array.from(onlineUsers.values()));
+    });
+  });
+
+
+//listen port
+server.listen(port, ()=> {
     console.log("everything is ok..!")
 }) 

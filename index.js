22455e594b1699337bdf39b6b69b1acd45afa708
Gurkan Yakar <data.yakar@gmail.com>
2023-06-03 16:21:44 +0300

    add express

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req,res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-    res.write('Successfull connection ! ');
-    res.end();
-});
+// const server = http.createServer((req,res) => {
+//     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
+//     res.write('Successfull connection ! ');
+//     res.end();
+// });
 
-server.listen(3000, () => {
-    console.log('App started !')
+// server.listen(3000, () => {
+//     console.log('App started !')
+// })
+
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('test message ... !');
 })
+
+app.get('/chat', (req, res) => {
+    res.send('you are in chat page...!');
+   });
+
+app.listen(3000, ()=> {
+    console.log("everything is ok..!")
+}) 

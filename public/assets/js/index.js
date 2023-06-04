e46552a80b57450133f64c969ed94a4c7355fa30
Gurkan Yakar <data.yakar@gmail.com>
2023-06-05 02:45:59 +0300

    image send

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,6 +3,13 @@ const mainContainer = document.getElementById("main-container");
 const connectBtn = document.getElementById("connectBtn");
 const sendBtn = document.getElementById("sendBtn");
 const messageInput = document.getElementById("messageInput");
+
+const emojiBtn = document.getElementById("emojiBtn");
+const emojiPanel = document.getElementById("emojiPanel");
+const emojiContainer = document.querySelector(".emoji-container");
+
+const fileInput = document.getElementById("fileInput");
+
 let globalUserName;
 
 connectBtn.addEventListener("click", () => {
@@ -12,9 +19,7 @@ connectBtn.addEventListener("click", () => {
   if (username.trim() !== "") {
     socket.emit("join", username);
 
-    mainContainer.removeChild(
-      document.getElementById("user-login-block")
-    );
+    mainContainer.removeChild(document.getElementById("user-login-block"));
     document.getElementById("online-users").style.display = "block";
     document.getElementById("chat-card").style.display = "block";
   }
@@ -22,7 +27,7 @@ connectBtn.addEventListener("click", () => {
 
 socket.on("userList", (userList) => {
   const userListElement = document.getElementById("userList");
-  userListElement.innerHTML = ""; // Önceki kullanıcı listesini temizle
+  userListElement.innerHTML = "";
 
   userList.forEach((username) => {
     const listItem = document.createElement("li");
@@ -38,43 +43,63 @@ socket.on("userList", (userList) => {
 });
 
 socket.on("chat message", (data) => {
-const globalChat = document.getElementById("globalChat");
-const listItem = document.createElement("li");
-listItem.classList.add("chat-item");
+  const globalChat = document.getElementById("globalChat");
+  const listItem = document.createElement("li");
+  listItem.classList.add("chat-item");
 
-const messageContent = document.createElement("div");
-messageContent.classList.add("message-content");
+  const messageContent = document.createElement("div");
+  messageContent.classList.add("message-content");
 
-const senderInfo = document.createElement("span");
-senderInfo.classList.add("sender-info");
-senderInfo.textContent = data.sender + ": ";
+  const senderInfo = document.createElement("span");
+  senderInfo.classList.add("sender-info");
+  senderInfo.textContent = data.sender + ": ";
 
-const messageText = document.createElement("span");
-messageText.classList.add("message-text");
-messageText.textContent = data.message;
+  const messageText = document.createElement("span");
+  messageText.classList.add("message-text");
+  messageText.textContent = data.message;
 
-const timeInfo = document.createElement("span");
-timeInfo.classList.add("time-info");
-const currentTime = new Date();
-const formattedTime = currentTime.toLocaleTimeString("en-US", {
-hour: "numeric",
-minute: "numeric",
-hour12: true
+  const timeInfo = document.createElement("span");
+  timeInfo.classList.add("time-info");
+  const currentTime = new Date();
+  const formattedTime = currentTime.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  timeInfo.textContent = formattedTime;
+
+  messageContent.appendChild(senderInfo);
+
+  if (data.file) {
+    const fileImage = document.createElement("img");
+    fileImage.src = data.file.content;
+    fileImage.alt = data.file.name;
+    fileImage.style.maxWidth = "100%";
+    fileImage.style.maxHeight = "200px";
+    messageContent.appendChild(fileImage);
+  }
+
+  messageContent.appendChild(messageText);
+  messageContent.appendChild(timeInfo);
+  listItem.appendChild(messageContent);
+
+  globalChat.appendChild(listItem);
 });
-timeInfo.textContent = formattedTime;
 
-messageContent.appendChild(senderInfo);
-messageContent.appendChild(messageText);
-messageContent.appendChild(timeInfo);
-listItem.appendChild(messageContent);
-globalChat.appendChild(listItem);
-});
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-sendBtn.addEventListener("click", () => {
-  const message = messageInput.value;
-  if (message.trim() !== "") {
-    const isGlobal = true;
+  const reader = new FileReader();
+  const file = fileInput.files[0];
 
+  if (!file && !messageInput.value) {
+    alert("Lütfen bir mesaj girin");
+    return;
+  }
+
+  const message = messageInput.value.trim();
+
+  if (message !== "" || file) {
     const timeInfo = document.createElement("span");
     timeInfo.classList.add("time-info");
     const currentTime = new Date();
@@ -85,9 +110,42 @@ sendBtn.addEventListener("click", () => {
     });
     timeInfo.textContent = formattedTime;
 
-    socket.emit("chat message", { sender: globalUserName, message }); // Gönderenin kullanıcı adını iletiyoruz
+    const data = { sender: globalUserName, message };
+
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        data.file = {
+          name: file.name,
+          type: file.type,
+          size: file.size,
+          content: reader.result,
+        };
+        socket.emit("chat message", data);
+      };
+    } else {
+      socket.emit("chat message", data);
+    }
+
     messageInput.value = "";
+    fileInput.value = "";
   }
 });
 
+const emoji = ["😀", "😁", "😂", "🤣", "😃", "😄", "😅", "😆", "😉", "😊", "😋", "😎", "😍", "😘", "😗", "😙", "😚"];
 
+emojiBtn.addEventListener("click", () => {
+  emojiPanel.classList.toggle("d-none");
+});
+
+emoji.forEach((e) => {
+  const emojiButton = document.createElement("button");
+  emojiButton.classList.add("emoji-button");
+  emojiButton.textContent = e;
+  emojiButton.addEventListener("click", () => {
+    const currentMessage = messageInput.value;
+    messageInput.value = currentMessage + e;
+    emojiPanel.classList.add("d-none");
+  });
+  emojiContainer.appendChild(emojiButton);
+});
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const socketio = socket(server);
 const port = 3000;
 
-//start index html
+// İndex HTML'i başlat
 const publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
 
@@ -16,34 +16,40 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-const onlineUsers = new Map(); //online user list
+const onlineUsers = new Map(); // çevrimiçi kullanıcı listesi
 
 socketio.on('connection', (socket) => {
-    socket.on('join', (username) => {
-        onlineUsers.set(socket.id, username); // add new user to list
-        console.log(username + ' connected to server..!')
-        socketio.emit('userList', Array.from(onlineUsers.values()));
-    });
-    //client disconnect delete from list
-    socket.on('disconnect', () => {
-        const username = onlineUsers.get(socket.id); // get username using socket id
-        onlineUsers.delete(socket.id); //delete user to list
-        console.log(username + ' disconnected to server..!')
-        socketio.emit('userList', Array.from(onlineUsers.values()));
-    });
-
-    socket.on("chat message", (data) => {
-      const { sender, message } = data;
-      console.log("Gönderen: " + sender);
-      console.log("Mesaj: " + message);
-      socketio.emit("chat message", { sender, message, isGlobal: true });
-        });
-    
-    
+  socket.on('join', (username) => {
+    onlineUsers.set(socket.id, username); // yeni kullanıcıyı listeye ekle
+    console.log(username + ' sunucuya bağlandı..!')
+    socketio.emit('userList', Array.from(onlineUsers.values()));
   });
 
+  // İstemci bağlantısı kesildiğinde listeyi güncelle ve kullanıcıyı sil
+  socket.on('disconnect', () => {
+    const username = onlineUsers.get(socket.id); // soket kimliğini kullanarak kullanıcı adını al
+    onlineUsers.delete(socket.id); // kullanıcıyı listeden sil
+    console.log(username + ' sunucudan ayrıldı..!')
+    socketio.emit('userList', Array.from(onlineUsers.values()));
+  });
 
-//listen port
-server.listen(port, ()=> {
-    console.log("everything is ok..!")
-}) 
+  socket.on('chat message', (msg) => {
+    const { sender, message, file } = msg;
+
+    const data = {
+      sender: sender,
+      message: message,
+      file: file ? { name: file.name, type: file.type, content: file.content } : null
+    };
+
+    socketio.emit('chat message', data);
+
+    // Mesajı kullanmak için istediğiniz işlemleri burada yapabilirsiniz
+  });
+
+});
+
+// Port dinle
+server.listen(port, () => {
+  console.log("Her şey yolunda..!")
+});
